59844b265193b7b819fec8d85b160e22d3ecb83e
Barry Grundy <bgrundy@linuxleo.com>
2026-03-09 01:52:13 -0400

    update checkconflict script to copy from SBo if there is an md5 mismatch

--- CheckConflict.sh
+++ CheckConflict.sh
@@ -4,12 +4,15 @@ LOCAL_GIT='./'
 SBO_REPO='/usr/sbo/repo'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
+MISMATCH=0
 
 # Check the MD5 of SBo repo vs. Local Repo
 echo
-SBO=$(sudo sbofind -te $1 | awk -F'Path:' '{print $1}')
+SBO=$(sudo sbofind -te $1 | grep SBo)
 SBO_PATH=$(sudo sbofind -te $1 | grep Path | awk '{print $NF}')
-echo $SBO
+echo "$SBO"
+LOCAL_VER=$(cat libvhdi/libvhdi.info | grep VERSION | awk -F'"' '{print $2}')
+echo -e "GIT:\t$1 $LOCAL_VER"
 echo
 echo "Download URL:"
 sudo sbofind -tei $1 | grep DOWNLOAD= | awk -F '"' '{print $2}'
@@ -17,18 +20,23 @@ echo
 echo "Home Page:"
 sudo sbofind -tei $1 | grep HOMEPAGE= | awk -F '"' '{print $2}'
 echo
+
+
+# TEST the MD5sums:
 if [[ $(md5sum $1/README | awk '{print $1}') == $(md5sum $SBO_PATH/README | awk '{print $1}') ]]
 then
     echo "README matches"
 else
     printf "${RED}README MIS-MATCH${NC}\n"
+    MISMATCH=1
 fi
 
-if [[ $(md5sum $1/$1.SlackBuild | awk '{print $1}') == $(md5sum $SBO_PATH/$1.SlackBuild | awk '{print $1}') ]]
+if [[ $(md5sum $1/$1.SlackBuild | awk '{print $1}') == $(md5sum $SBO_PATH/$1.SlackBuild | awk '{print $1}') ]] 
 then
     echo "$1.SlackBuild matches"
 else
     printf "${RED}$1.SlackBuild MIS-MATCH${NC}\n"
+    MISMATCH=1
 fi
 
 if [[ $(md5sum $1/$1.info | awk '{print $1}') == $(md5sum $SBO_PATH/$1.info | awk '{print $1}') ]]
@@ -36,6 +44,7 @@ then
     echo "$1.info matches"
 else
     printf "${RED}$1.info MIS-MATCH${NC}\n"
+    MISMATCH=1
 fi
 
 if [[ $(md5sum $1/slack-desc | awk '{print $1}') == $(md5sum $SBO_PATH/slack-desc | awk '{print $1}') ]]
@@ -43,4 +52,40 @@ then
     echo "slack-desc matches"
 else
     printf "${RED}slack-desc MIS-MATCH${NC}\n"
+    MISMATCH=1
 fi
+
+echo
+
+if [[ $MISMATCH == 1 ]]
+then
+    printf "${RED}Mismatch${NC}: "
+else
+    echo "No mismatch."
+    exit 0
+fi
+
+# If there is a mis-match, allow copying from SBo repo:
+
+while true; do
+    # -p allows specifying a prompt
+    read -p "Do you want to copy from SBo repo? (y/n): " yn
+    case $yn in
+        # Case-insensitive match for 'y' or 'yes'
+        [Yy]* ) echo "Proceeding..."
+        cp  $SBO_PATH/* $1/.
+        echo "Copy Complete";
+        break
+        ;;
+
+        # Case-insensitive match for 'n' or 'no'
+        [Nn]* ) echo "Exiting..." 
+        exit
+        ;;
+        # Default case for invalid input, asks again
+        * ) echo "Invalid response. Please answer yes or no."
+        ;;
+    esac
+done
+
+
